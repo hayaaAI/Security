a17d20147e36d8734ef9fe022caa3badbd281dd1
hayaa <dev@hayaa-ai.com>
2018-09-07 03:44:56 +0800

    项目解耦和

---
+++ Hayaa.Security.Service/Client/C#/Hayaa.WebSecurity.Client/Model/PersonGrant.cs
@@ -0,0 +1,16 @@
+﻿using Hayaa.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.UserAuth.Service
+{
+   public class PersonGrant:BaseData
+    {
+        public int AppGrantId { set; get; }
+        public int AppId { set; get; }
+        public int UserId { set; get; }
+        public int AppFunctionId { set; get; }
+
+    }
+}
